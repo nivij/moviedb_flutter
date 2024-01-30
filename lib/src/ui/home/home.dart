5495014997418fbb21885b354cf8a7d20266156d
nivij <nivijsuresh@gmail.com>
2024-01-30 17:08:19 +0530

    shimmerLoading-CarouselSlider

--- lib/src/ui/home/home.dart
+++ lib/src/ui/home/home.dart
@@ -1,5 +1,8 @@
-
-
+import 'package:codenoramovie/src/ui/home/widgets/dummyLoading.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:codenoramovie/src/bloc/moviebloc/movie_bloc.dart';
 import 'package:codenoramovie/src/bloc/moviebloc/movie_bloc_event.dart';
 import 'package:codenoramovie/src/bloc/moviebloc/movies_bloc_state.dart';
@@ -7,11 +10,6 @@ import 'package:codenoramovie/src/res/images/assetsImage.dart';
 import 'package:codenoramovie/src/res/widgets/watchButton.dart';
 import 'package:codenoramovie/src/ui/home/widgets/carouselSlider.dart';
 import 'package:codenoramovie/src/ui/home/widgets/trending_card.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import '../../res/buttons/appbar.dart';
 import '../../res/buttons/list_button.dart';
 import '../../res/string/string.dart';
@@ -30,29 +28,33 @@ class HomePage extends HookWidget {
     }, []);
 
     void handleWatchButtonTap() {
-      // Your logic for the Watch button tap
+      //  logic for the Watch button tap
       print("Watch button tapped!");
     }
+
+    Future<void> onRefresh() async {
+      //  logic to refresh the data
+      movieBloc.add(MovieEventStarted(0, ""));
+    }
+
     Widget getMovieText(int index, bool isLoading) {
       if (state is MovieLoaded && index >= 0 && index < state.movielist.length) {
         print(state.movielist[index].title);
         return Text(
-          state.movielist[index].originalTitle,
+          state.movielist[currentImageIndex.value].originalTitle,
           style: GoogleFonts.outfit(color: Colors.white),
         );
       }
-      return isLoading ? ShimmerLoadingWidget(width: 90,height: 20,) : Text('');
+      return isLoading ? ShimmerLoadingWidget(width: 90, height: 20,) : Text('');
     }
 
-
-    Widget getMovieGenre(int index,bool isLoading) {
+    Widget getMovieGenre(int index, bool isLoading) {
       if (state is MovieLoaded && index >= 0 &&
           index < state.movielist.length) {
         return Text(
-          state.movielist[index].title,
+          state.movielist[currentImageIndex.value].title,
           style: GoogleFonts.outfit(color: Colors.white.withOpacity(0.6)),
-        )
-        ;
+        );
       }
       return isLoading ? ShimmerLoadingWidget(width: 70, height: 20,) : Text('');
     }
@@ -69,80 +71,72 @@ class HomePage extends HookWidget {
         ],
       ),
       backgroundColor: Color(0xFF121318),
-      body:
-      SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children:[
-            BlocBuilder<MovieBloc, MovieState>(
-              builder: (context, state) {
-                if (state is MovieLoaded) {
-                  return  CarosuelSliderWidget(
-                    imageUrls: state.movielist
-                        .map((movie) => buildImageUrl(movie.backdropPath))
-                        .toList(),
-                    onImageChanged: (index) {
-                      print("Index changed: $index");
-                      print("Image list length: ${image.length}");
-                      if (index >= 0 && index < state.movielist.length) {
-                        currentImageIndex.value = index;
-                      }
-                    },
-                  );
-
-
-                } else if (state is MovieLoading) {
-                  return CircularProgressIndicator();
-                } else if (state is MovieError) {
-                  return Text('Error fetching movies');
-                } else {
-                  return Text('Unknown state');
-                }
-              },
-            ),
-
-            // CarosuelSliderWidget(
-            //   onImageChanged: (index) {
-            //     currentImageIndex.value = index;
-            //   },
-            // ),
-            kheight20,
-            getMovieText(currentImageIndex.value, isLoading),
-            kheight10,
-           getMovieGenre(currentImageIndex.value, isLoading),
-            kheight20,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Expanded(
-                  child: My_List_Button(
-                    icon: Icons.info_outline,
-                    title: 'Details',
-                    IconSize: 23,
-                    textSize: 15,
+      body: RefreshIndicator(
+        onRefresh: onRefresh,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              BlocBuilder<MovieBloc, MovieState>(
+                builder: (context, state) {
+                  if (state is MovieLoaded) {
+                    return CarosuelSliderWidget(
+                      imageUrls: state.movielist
+                          .map((movie) => buildImageUrl(movie.backdropPath))
+                          .toList(),
+                      onImageChanged: (index) {
+                        print("Index changed: $index");
+                        print("Image list length: ${image.length}");
+                        if (index >= 0 && index < state.movielist.length) {
+                          currentImageIndex.value = index;
+                        }
+                      },
+                    );
+                  } else if (state is MovieLoading) {
+                    return DummyLoading();
+                  } else if (state is MovieError) {
+                    return Text('Error fetching movies');
+                  } else {
+                    return Text('Unknown state');
+                  }
+                },
+              ),
+              kheight20,
+              getMovieText(currentImageIndex.value, isLoading),
+              kheight10,
+              getMovieGenre(currentImageIndex.value, isLoading),
+              kheight20,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: My_List_Button(
+                      icon: Icons.info_outline,
+                      title: 'Details',
+                      IconSize: 23,
+                      textSize: 15,
+                    ),
                   ),
-                ),
-                WatchButton(onTap: handleWatchButtonTap),
-                Expanded(
-                  child: My_List_Button(
-                    icon: Icons.bookmark,
-                    title: 'watchList',
-                    IconSize: 23,
-                    textSize: 15,
+                  WatchButton(onTap: handleWatchButtonTap),
+                  Expanded(
+                    child: My_List_Button(
+                      icon: Icons.bookmark,
+                      title: 'watchList',
+                      IconSize: 23,
+                      textSize: 15,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            kheight20,
-            main_title_card(title: "Now Playing"),
-            kheight20,
-            main_title_card(title: "Released in the past year"),
-            kheight20,
-          ],
+                ],
+              ),
+              kheight20,
+              main_title_card(title: "Now Playing"),
+              kheight20,
+              main_title_card(title: "Released in the past year"),
+              kheight20,
+            ],
+          ),
         ),
       ),
     );
   }
-
-
 }
---
+++ lib/src/ui/home/widgets/dummyLoading.dart
@@ -0,0 +1,44 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../res/widgets/cardloading.dart';
+
+class DummyLoading extends StatelessWidget {
+  const DummyLoading({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.11),
+            spreadRadius: 1,
+            blurRadius: 3,
+            offset: Offset(10, 40),
+          ),
+        ],
+      ),
+      child: CarouselSlider(
+        options: CarouselOptions(
+          viewportFraction: 0.5,
+          height: 330,
+          aspectRatio: 2.0,
+          enlargeCenterPage: true,
+          autoPlay: true,
+          autoPlayInterval: Duration(seconds: 5),
+        ),
+        items: [
+          Container(
+            width: double.infinity,
+            child:ShimmerLoadingWidget()
+          ),
+        ],
+      ),
+    );
+  }
+}
