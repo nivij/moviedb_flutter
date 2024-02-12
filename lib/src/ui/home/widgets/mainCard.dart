9fc685d56a3ffbf4e8fc4ab07deb85d4f9afd73c
nivij <nivijsuresh@gmail.com>
2024-02-12 13:14:34 +0530

    added shimmerloading for nowplaying

--- lib/src/ui/home/widgets/mainCard.dart
+++ lib/src/ui/home/widgets/mainCard.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../../res/widgets/cardloading.dart';
+
 class maincard extends StatelessWidget {
   const maincard({Key? key, required this.index, required this.imageUrls}) : super(key: key);
 
@@ -23,7 +25,7 @@ class maincard extends StatelessWidget {
           child: CachedNetworkImage(
             imageUrl: url,
             fit: BoxFit.cover,
-            placeholder: (context, url) => Center(child: CircularProgressIndicator()), // Placeholder widget while loading
+            placeholder: (context, url) => Center(child: ShimmerLoadingWidget(width: 150,height: 220,)), // Placeholder widget while loading
             errorWidget: (context, url, error) => Icon(Icons.error), // Error widget when image fails to load
           ),
         );
